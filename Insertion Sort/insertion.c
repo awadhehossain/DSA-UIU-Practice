8b9e6ca825e8c99c1780e8f7d32fab5f85b4d5cf
Md. Awadhe Hossain <149267112+awadhehossain@users.noreply.github.com>
2026-04-04 02:23:58 +0600

    Insertion sort

--- Insertion Sort/insertion.c
+++ Insertion Sort/insertion.c
@@ -1,5 +1,47 @@
 #include<stdio.h>
+
+void insertion_sort(int arr[],int n)
+{
+    for(int i=1;i<n;i++)
+    {
+        int key=arr[i];
+        int j;
+        for(j=i-1;j>=0 && arr[j]>key;j--)
+        {
+            arr[j+1]=arr[j];
+        }
+        arr[j+1]=key;
+    }
+}
+
 int main()
 {
+    int n;
+    printf("Enter the array size: ");
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+
+    printf("Initial Array: ");
+
+    for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
+    printf("\n");
+
+    insertion_sort(arr,n);
+
+    printf("Sorted Array: ");
+
+     for(int i=0;i<n;i++)
+    {
+        printf("%d ",arr[i]);
+    }
+
     return 0;
 }
